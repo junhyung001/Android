15e4c3500b0e93a2d5127672066d5bc6f640aca2
PC_13\user <cjh82350@naver.com>
2021-06-16 20:22:59 +0900

    로그인 방법, 로그인 화면 구현

---
+++ app/src/main/java/com/example/retrofit2study/scenarios/signin/SignInActivity.kt
@@ -0,0 +1,48 @@
+package com.example.retrofit2study.scenarios.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.retrofit2study.R
+import com.example.retrofit2study.api.connector.ServerConnector
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var edtUsername: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignIn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        initUI()
+    }
+
+    private fun initUI(){
+        edtUsername = findViewById(R.id.edt_signIn_username)
+        edtPassword = findViewById(R.id.edt_signIn_password)
+        btnSignIn = findViewById(R.id.btn_singIn)
+    }
+
+    private fun initListener(){
+        btnSignIn.setOnClickListener{
+            val username = edtUsername.text.toString()
+            val password = edtPassword.text.toString()
+
+            ServerConnector.getSignIn(username, password){
+                if(it == null)
+                    showToast("서버 오류가 발생하였습니다.")
+                else if(it?.code != 0)
+                    showToast(it.msg)
+                else
+                    showToast("로그인 성공!")
+            }
+        }
+    }
+    private fun showToast(msg: String){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+}
